1108641ffd5bd218bbd1bdefc68c08944ec8b3b9
Javier Moreno <javiermorenoprieto@gmail.com>
2025-03-22 13:09:40 +0100

    Add "Go to top" button and update personal description in index.html

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
