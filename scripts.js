161ead463383da2b5e2fb2ee601ddae1ec4aa4f9
Javier Moreno <javiermorenoprieto@gmail.com>
2025-03-26 23:44:55 +0100

    Add dark mode toggle and enhance "Go to top" button functionality

--- scripts.js
+++ scripts.js
@@ -1,18 +1,59 @@
-// Get the button
-var mybutton = document.getElementById("topBtn");
+const topBtn = document.getElementById("topBtn");
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('darkmodebutton');
 
 // When the user scrolls down 100px from the top of the document, show the button
 window.onscroll = function() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        mybutton.style.display = "block";
+        topBtn.style.visibility = "visible";
+        topBtn.style.opacity = "1";
+        topBtn.style.pointerEvents = "auto";
     } else {
-        mybutton.style.display = "none";
+        topBtn.style.visibility = "hidden";
+        topBtn.style.opacity = "0";
+        topBtn.style.pointerEvents = "none";
+    }
+
+    if (document.body.scrollTop < 0 || document.documentElement.scrollTop < 450) {
+        themeSwitch.style.visibility = "visible";
+        themeSwitch.style.opacity = "1";
+        themeSwitch.style.pointerEvents = "auto";
+    } else {
+        themeSwitch.style.visibility = "hidden";
+        themeSwitch.style.opacity = "0";
+        themeSwitch.style.pointerEvents = "none";
     }
 };
 
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", function() {
+topBtn.addEventListener("click", function() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.button'));
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode();
+}
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'enabled');
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+};
+
+if (darkmode === 'enabled') {
+    enableDarkMode();
+}
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode');
+    if (darkmode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
